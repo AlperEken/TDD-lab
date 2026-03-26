2ac6bb18afc82f1e5752e1fdd14046125199e01b
AlperEken <alpereken04@gmail.com>
2026-03-26 22:27:57 +0100

    CLOCKTESTTTT!!!!

--- Lab/src/ClockTest.java
+++ Lab/src/ClockTest.java
@@ -1,260 +1,245 @@
 //By Alper Eken and Samuel Schulze
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 
 class ClockTest {
 
+    private Clock clock;
+
+    @BeforeEach
+    void setUp() {
+        clock = new Clock();
+    }
 
     @Test
-    void readyTestTime()
-    {
-        Clock clock = new Clock();
-
+    void testS1toS3Ready() {
         assertEquals(States.Change_Time.toString(), clock.ready());
     }
 
     @Test
-    void readyTestDateInvalid(){
-        Clock clock = new Clock();
-
-        clock.ready();
-
-        assertEquals("Invalid state", clock.ready());
-    }
-
-    @Test
-    void readyTestDate()
-    {
-        Clock clock = new Clock();
-
+    void testS2toS4Ready() {
         clock.changeMode();
-
         assertEquals(States.Change_Date.toString(), clock.ready());
-
     }
 
-
     @Test
-    void changeModeS1toS2(){
-        Clock clock = new Clock();
-
+    void testS1toS2ChangeMode() {
         assertEquals(States.Display_Date.toString(), clock.changeMode());
     }
 
     @Test
-    void changeModeS2toS1(){
-        Clock clock = new Clock();
-
+    void testS2toS1ChangeMode() {
         clock.changeMode();
-
         assertEquals(States.Display_Time.toString(), clock.changeMode());
     }
 
     @Test
-    void changeModeInvalid(){
-        Clock clock = new Clock();
-
+    void testS3toS1SetValid() {
         clock.ready();
-
-        assertEquals("Invalid state", clock.changeMode());
+        assertEquals("10:15:30", clock.set(10, 15, 30));
     }
 
     @Test
-    void testTimesetExaktMinHour(){
-        Clock clock = new Clock();
+    void testS4toS1SetValid() {
+        clock.changeMode(); clock.ready();
+        assertEquals("2025-12-24", clock.set(2025, 12, 24));
+    }
+
+    @Test
+    void testS3toS3IllegalReady() {
+        clock.ready();
+        assertEquals("Already in setting mode", clock.ready());
+    }
+
+    @Test
+    void testS4toS4IllegalReady() {
+        clock.changeMode(); clock.ready();
+        assertEquals("Already in setting mode", clock.ready());
+    }
+
+    @Test
+    void testS3IllegalChangeMode() {
+        clock.ready();
+        assertEquals("Cannot change mode while in setting mode", clock.changeMode());
+    }
+
+    @Test
+    void testS4IllegalChangeMode() {
+        clock.changeMode(); clock.ready();
+        assertEquals("Cannot change mode while in setting mode", clock.changeMode());
+    }
+
+    @Test
+    void testS1IllegalSet() {
+        assertEquals("Must be in a setting mode to set values", clock.set(12, 0, 0));
+    }
+
+    @Test
+    void testS2IllegalSet() {
+        clock.changeMode();
+        assertEquals("Must be in a setting mode to set values", clock.set(2024, 1, 1));
+    }
+
+    @Test
+    void testS3HourMinBoundary() {
         clock.ready();
         assertEquals("0:0:0", clock.set(0, 0, 0));
     }
 
     @Test
-    void testTimesetUnderMinHour(){
-        Clock clock = new Clock();
+    void testS3HourUnderMinBoundary() {
         clock.ready();
-        assertEquals("-1:0:0", clock.set(-1, 0, 0));
+        assertEquals("Invalid hour: -1. Must be 0-23", clock.set(-1, 0, 0));
     }
 
     @Test
-    void testTimesetExaktMaxHour(){
-        Clock clock = new Clock();
+    void testS3HourMaxBoundary() {
         clock.ready();
         assertEquals("23:0:0", clock.set(23, 0, 0));
     }
 
     @Test
-    void testTimesetOverMaxHour(){
-        Clock clock = new Clock();
+    void testS3HourOverMaxBoundary() {
         clock.ready();
-        assertEquals("-1:0:0", clock.set(25, 0, 0));
+        assertEquals("Invalid hour: 25. Must be 0-23", clock.set(25, 0, 0));
     }
 
     @Test
-    void testTimesetExaktMinMinute(){
-        Clock clock = new Clock();
+    void testS3MinuteMinBoundary() {
         clock.ready();
         assertEquals("0:0:0", clock.set(0, 0, 0));
     }
 
     @Test
-    void testTimesetUnderMinMinute(){
-        Clock clock = new Clock();
+    void testS3MinuteUnderMinBoundary() {
         clock.ready();
-        assertEquals("0:-1:0", clock.set(0, -1, 0));
+        assertEquals("Invalid minute: -1. Must be 0-59", clock.set(0, -1, 0));
     }
 
     @Test
-    void testTimesetExaktMaxMinute(){
-        Clock clock = new Clock();
+    void testS3MinuteMaxBoundary() {
         clock.ready();
         assertEquals("0:59:0", clock.set(0, 59, 0));
     }
 
     @Test
-    void testTimesetOverMaxMinute(){
-        Clock clock = new Clock();
+    void testS3MinuteOverMaxBoundary() {
         clock.ready();
-        assertEquals("0:-1:0", clock.set(0, 61, 0));
+        assertEquals("Invalid minute: 61. Must be 0-59", clock.set(0, 61, 0));
     }
 
     @Test
-    void testTimesetExaktMinSecond(){
-        Clock clock = new Clock();
+    void testS3SecondMinBoundary() {
         clock.ready();
         assertEquals("0:0:0", clock.set(0, 0, 0));
     }
 
     @Test
-    void testTimesetUnderMinSecond(){
-        Clock clock = new Clock();
+    void testS3SecondUnderMinBoundary() {
         clock.ready();
-        assertEquals("0:0:-1", clock.set(0, 0, -1));
+        assertEquals("Invalid second: -1. Must be 0-59", clock.set(0, 0, -1));
     }
 
     @Test
-    void testTimesetExaktMaxSecond(){
-        Clock clock = new Clock();
+    void testS3SecondMaxBoundary() {
         clock.ready();
         assertEquals("0:0:59", clock.set(0, 0, 59));
     }
 
     @Test
-    void testTimesetOverMaxSecond(){
-        Clock clock = new Clock();
+    void testS3SecondOverMaxBoundary() {
         clock.ready();
-        assertEquals("0:0:-1", clock.set(0, 0, 61));
+        assertEquals("Invalid second: 61. Must be 0-59", clock.set(0, 0, 61));
     }
 
     @Test
-    void testDatesetExaktMinYear(){
-        Clock clock = new Clock();
+    void testS4YearMinBoundary() {
         clock.changeMode(); clock.ready();
         assertEquals("2000-1-1", clock.set(2000, 1, 1));
     }
 
     @Test
-    void testDatesetUnderMinYear(){
-        Clock clock = new Clock();
+    void testS4YearUnderMinBoundary() {
         clock.changeMode(); clock.ready();
-        assertEquals("-1-1-1", clock.set(1999, 1, 1));
+        assertEquals("Invalid year: 1999. Must be 2000-2100", clock.set(1999, 1, 1));
     }
 
     @Test
-    void testDatesetExaktMaxYear(){
-        Clock clock = new Clock();
+    void testS4YearMaxBoundary() {
         clock.changeMode(); clock.ready();
         assertEquals("2100-1-1", clock.set(2100, 1, 1));
     }
 
     @Test
-    void testDatesetOverMaxYear(){
-        Clock clock = new Clock();
+    void testS4YearOverMaxBoundary() {
         clock.changeMode(); clock.ready();
-        assertEquals("-1-1-1", clock.set(2101, 1, 1));
+        assertEquals("Invalid year: 2101. Must be 2000-2100", clock.set(2101, 1, 1));
     }
 
     @Test
-    void testDatesetExaktMinMonth(){
-        Clock clock = new Clock();
+    void testS4MonthMinBoundary() {
         clock.changeMode(); clock.ready();
         assertEquals("2000-1-1", clock.set(2000, 1, 1));
     }
 
     @Test
-    void testDatesetUnderMinMonth(){
-        Clock clock = new Clock();
+    void testS4MonthUnderMinBoundary() {
         clock.changeMode(); clock.ready();
-        assertEquals("2000--1-1", clock.set(2000, 0, 1));
+        assertEquals("Invalid month: 0. Must be 1-12", clock.set(2000, 0, 1));
     }
 
     @Test
-    void testDatesetExaktMaxMonth(){
-        Clock clock = new Clock();
+    void testS4MonthMaxBoundary() {
         clock.changeMode(); clock.ready();
         assertEquals("2000-12-1", clock.set(2000, 12, 1));
     }
 
     @Test
-    void testDatesetOverMaxMonth(){
-        Clock clock = new Clock();
+    void testS4MonthOverMaxBoundary() {
         clock.changeMode(); clock.ready();
-        assertEquals("2000--1-1", clock.set(2000, 13, 1));
+        assertEquals("Invalid month: 13. Must be 1-12", clock.set(2000, 13, 1));
     }
 
     @Test
-    void testDatesetExaktMinDay(){
-        Clock clock = new Clock();
+    void testS4DayMinBoundary() {
         clock.changeMode(); clock.ready();
         assertEquals("2000-1-1", clock.set(2000, 1, 1));
     }
 
     @Test
-    void testDatesetUnderMinDay(){
-        Clock clock = new Clock();
+    void testS4DayUnderMinBoundary() {
         clock.changeMode(); clock.ready();
-        assertEquals("2000-1--1", clock.set(2000, 1, 0));
+        assertEquals("Invalid day: 0. Must be 1-31", clock.set(2000, 1, 0));
     }
 
     @Test
-    void testDatesetExaktMaxDay(){
-        Clock clock = new Clock();
+    void testS4DayMaxBoundary() {
         clock.changeMode(); clock.ready();
         assertEquals("2000-1-31", clock.set(2000, 1, 31));
     }
 
     @Test
-    void testDatesetOverMaxDay(){
-        Clock clock = new Clock();
+    void testS4DayOverMaxBoundary() {
         clock.changeMode(); clock.ready();
-        assertEquals("2000-1--1", clock.set(2000, 1, 32));
+        assertEquals("Invalid day: 32. Must be 1-31", clock.set(2000, 1, 32));
     }
 
     @Test
-    void testTimesetExactly24Hour() {
-        Clock clock = new Clock();
+    void testS3Exactly24Hour() {
         clock.ready();
         assertEquals("0:0:0", clock.set(24, 0, 0));
     }
 
     @Test
-    void testTimesetExactly60Minute() {
-        Clock clock = new Clock();
+    void testS3Exactly60Minute() {
         clock.ready();
         assertEquals("0:0:0", clock.set(0, 60, 0));
     }
 
     @Test
-    void testTimesetExactly60Second() {
-        Clock clock = new Clock();
+    void testS3Exactly60Second() {
         clock.ready();
         assertEquals("0:0:0", clock.set(0, 0, 60));
     }
-
-    @Test
-    void testSetInDisplayDate() {
-        Clock clock = new Clock();
-        clock.changeMode();
-        assertEquals("Invalid state", clock.set(2024, 1, 1));
-    }
-
 }
